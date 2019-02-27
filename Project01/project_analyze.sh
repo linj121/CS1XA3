b3f7504abf247663c578000784c22a3cc2f6431d
linj121 <linj121@mcmaster.ca>
2019-02-27 00:25:37 +0000

    Part 2 finished

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 echo 'Hello,User.Enter the function you want to execute'
-echo 'Here are your options: TODOlog'
+echo 'Here are your options: TODOlog,DELtemp,SLEEP'
 echo 'OR you can enter Exit to exit the script'
 read -p 'Your choice is:' f
 echo Now executing $f
@@ -9,15 +9,33 @@ echo Now executing $f
 while [ $f != "Exit" ]
 do
     if [ $f = "TODOlog" ];then
-        cat /dev/null > ~/CS1XA3/Project01/todo.log
-        list=$( git ls-files )
-        for file in $list
-        do
-            while IFS= read line
-            do
-               echo "$line #TODO" >> ~/CS1XA3/Project01/todo.log
-            done < $file
-        done
+        [ -e todo.log ] && rm todo.log
+	cat ~/CS1XA3/Project01/todo.log | echo
+    elif [ $f = "DELtemp" ];then
+	untrack=$( git ls-files . exclude-standard others )
+	for file in $untrack
+	do
+	   if [[ "$file" == *.tmp ]];then
+              rm $file
+	      echo "Deleting untracked file: $file"
+           fi
+	done
+        echo "Deleting temporary files done"
+    elif [ $f = "GUESS" ];then
+         num=$[RANDOM%100+1]
+         echo "An integer from 1 to 100 has been generated.Guess what it is." 
+         while :
+         do
+            read -p "Please enter a number:" n
+            if [ $n -eq $num ];then
+                echo "Congratulations!You are right."
+                break
+            elif [ $n -gt $num ];then
+                echo "The right answer is smaller~"
+            else
+                echo "The right answer is bigger~"
+            fi
+         done
     else
         echo "Wrong command! Please enter the correct name."
     fi
---
+++ Project01/test.sh
@@ -0,0 +1,6 @@
+first blood #TODO
+double kill #TODO
+triple kill #TODO
+quadra kill #TODO
+penta kill #TODO
+hexa kill #TODO
